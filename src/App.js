4b272b59810e9c0f3887e272b9844fb5b8e0b734
adarsh16p7 <adarsh.patel0716@gmail.com>
2024-08-29 14:03:43 +0530

    First commit with a basic flow diagram.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactFlow, {
+  applyEdgeChanges,
+  applyNodeChanges,
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+} from 'react-flow-renderer';
+
+const initialElements = [
+  { id: '1', type: 'input', data: { label: 'Start Node' }, position: { x: 250, y: 5 } },
+  { id: '2', data: { label: 'Step 1' }, position: { x: 250, y: 100 } },
+  { id: '3', data: { label: 'Step 2' }, position: { x: 250, y: 200 } },
+  { id: 'e1-2', source: '1', target: '2', animated: true },
+  { id: 'e2-3', source: '2', target: '3' },
+];
 
 function App() {
+  const [nodes, setNodes] = useState(initialElements.filter((el) => !el.source && !el.target));
+  const [edges, setEdges] = useState(initialElements.filter((el) => el.source && el.target));
+
+  const onNodesChange = (changes) => setNodes((nds) => applyNodeChanges(changes, nds));
+  const onEdgesChange = (changes) => setEdges((eds) => applyEdgeChanges(changes, eds));
+  const onConnect = (params) => setEdges((eds) => addEdge(params, eds));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        deleteKeyCode={46} /* 'delete'-key */
+      >
+        <MiniMap />
+        <Controls />
+        <Background />
+      </ReactFlow>
     </div>
   );
 }
